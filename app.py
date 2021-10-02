3333cd6477c0d97fd8634845d0652bfe6c22d521
Curtis Dean <cdean.atx@gmail.com>
2021-10-01 19:53:03 -0500

    updated app.py

--- app.py
+++ app.py
@@ -8,3 +8,24 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 engine = create_engine('sqlite:///hawaii.sqlite')
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+app = Flask(__name__)
+@app.route('/')
+
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!</br></br>
+        Available Routes:</br></br>
+        /api/v1.0/precipitation</br>
+        /api/v1.0/stations</br>
+        /api/v1.0/tobs</br>
+        /api/v1.0/temp/start/end</br>
+        ''')
